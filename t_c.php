8d84ada088e8e52e9106f34561846c90d82fceaa
Samikshaa19 <50240495+Samikshaa19@users.noreply.github.com>
2021-01-25 22:54:45 +0530

    Add files via upload

---
+++ t_c.php
@@ -0,0 +1,50 @@
+
+<?php 
+
+    $active='T&C';
+    include("includes/header.php");
+
+?>
+<br><br>
+<p><h1>Terms & Conditions </h1> 
+By using this site the user has unconditionally accepted the terms and conditions of use as given here under and/or elsewhere in the site.
+
+All prices listed are in Indian Rupees for delivery in Mumbai City to Virar, Thane, Navi Mumbai up to Panvel*, Pune.
+<br><br>
+<h2>Ordering Policy</h2> <br>
+Please verify the time required for each product before ordering. If you have ordered without giving us a sufficient notice as per the indicated time in the product page, we would be unable to deliver the product. So please ensure that you read the terms and conditions against each product before ordering. We ensure that all the products published on this site are available for ordering, however in certain cases, if the ordered product is unavailable another product of equal value would be delivered. Also be aware that the weights indicated are approximate and might vary.
+<br><br>
+<h2>Refund Policy</h2><br>
+Since cakes are perishable goods and are made to order, we would not be able to provide any refunds. No refunds for wrong addresses, recipient unavailable and other causes beyond the control of us would not be entertained.
+<br><br>
+<h2>Shipping Policy</h2><br>
+While placing an order, please provide us with the following information for delivery at Hotel / Residence
+<br><br>
+- Hotel<br>
+
+- Full address of the hotel.
+<br>
+- Room No<br>
+
+- Telephone number of the hotel.
+<br>
+- Residence<br>
+
+- Home address with phone number.
+<br>
+- Proper landmark<br><br>
+
+<h2>Copyright and Trademark Policy</h2><br>
+All content on this website including the logo, graphics, text, design, is the property of Ribbons& Balloons.com and protected by copyright. No part of the content shall be copied, downloaded, uploaded, published and transmitted without prior, written permission from ribbons& balloons.com
+<br><br>
+<h2>Modification of Terms and conditions of Service:</h2><br>
+Ribbons& Balloons.com may at any time modify the terms and conditions ("Terms") of the Service without any prior notification to you and the revised version shall be available on the site. In the event of you not agreeing to the revised terms, then you should stop using the site. By continuous usage you are agreeing to the published Terms.
+
+</p>
+
+ <?php 
+    
+    include("includes/footer.php");
+    
+    ?>
+ 
